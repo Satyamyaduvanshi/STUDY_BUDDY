480d3540e0c23dd989de2e9bb4ea8817cbae3fd0
Satyam <ysatyam9627@gmail.com>
2025-02-01 13:44:13 +0530

    added authmiddleware

---
+++ studyBE/src/authMiddleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { CustomRequest } from "./interfaces/request";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const jwt_secret = process.env.JWT_SECRET || "";
+
+function auth(req: CustomRequest, res: Response, next: NextFunction) {
+  try {
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Unauthorized: No token provided" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decode = jwt.verify(token, jwt_secret) as JwtPayload;
+
+    if (!decode || typeof decode !== "object" || !decode.id) {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+
+    req.id = decode.id; 
+    next();
+  } catch (error) {
+    console.error("Auth Middleware Error:", error);
+    res.status(403).json({ message: "Forbidden: Invalid or expired token" });
+  }
+}
+
+export default auth;
+
