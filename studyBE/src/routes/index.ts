1d0512e9abafa839f3b52f0d70a1163e51d53e7e
Satyam <ysatyam9627@gmail.com>
2025-02-06 16:48:32 +0530

    initialize the vite project in repo

--- studyBE/src/routes/index.ts
+++ studyBE/src/routes/index.ts
@@ -3,6 +3,21 @@ import userRoutes from "./userRoutes"
 
 const router = Router();
 
+//* home route
+router.use("/",(req,res)=>{
+    try {
+        res.status(200).json({
+            message: " welcome to the STUDY BUDDY api homepage!!"
+        })
+        
+    } catch (e) {
+        res.status(500).json({
+            message: "internal server error"
+        })
+        return;
+        
+    }
+})
 
 //* user routes
 router.use("/api/user",userRoutes)
